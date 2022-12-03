a0e5eeda2493cb687fb1011fc52f9d757ba894c4
evumseu <evum95@gmail.com>
2022-12-03 15:40:29 +0800

    swj add new code

--- src/LoadingPage.js
+++ src/LoadingPage.js
@@ -1,29 +1,63 @@
 import { SEU_USER_ID } from "./OpenDotaDB"
 import { BrowserRouter, Routes, Route, useParams, useNavigate, json } from "react-router-dom"
 import { useState, useEffect } from "react"
+import { TextField, Button } from "@mui/material"
 
 function LoadUserData() {
 
+    const navigate = useNavigate()
     const params = useParams()
     const [profileData, setProfileData] = useState({})
     const [id, setID] = useState(!params.id ? params.id : SEU_USER_ID)
+
+    const handleSubmit=(event)=>{
+        event.preventDefault()
+        navigate(`/player/${id}`)
+    }
+
+    const onSearchChanged=(event)=> {
+        setID(event.target.value)
+    }
 
     useEffect(() => {
         fetchUserProfileData();
     }, [params.id]);
 
-    const fetchUserProfileData = async () => {
-        const userProfile = await fetch(`https://api.opendota.com/api/players/${id}`);
-        const user = await userProfile.json();
-        setProfileData(user);
-        console.log(user)
+    const fetchUserProfileData = () => {
+        fetch(`https://api.opendota.com/api/players/${id}`).then((Response) => Response.json()).then((data) => {
+            setProfileData(data)
+            console.log(data)
+        })
     };
 
 
     return <div>
-        <h2>Player Name: {profileData.profile.personaname}</h2>
-        <h2>MMR : {profileData.solo_competitive_rank}</h2>
-        <img src = {profileData.profile.avatarmedium}></img>
+        <form onSubmit={handleSubmit}>
+        <TextField
+            id="serach"
+            label="Search"
+            variant="outlined"
+            size="medium"
+            value={id}
+            onChange={onSearchChanged}
+        ></TextField> {' '}
+        <Button variant="contained" size="large" onClick={handleSubmit}>Search</Button>
+
+            {!profileData ? <div>
+                <h2>Player Name: {profileData.profile.personaname}</h2>
+                <h2>MMR : {profileData.mmr_estimate.estimate}</h2>
+                <img src={profileData.profile.avatarmedium}></img>
+            </div> : <p></p>}
+        </form>
+
+    </div>
+}
+
+function HomePage() {
+
+    return <div>
+        <h1>This is Homepage</h1>
+    
     </div>
 }
 
@@ -32,8 +66,8 @@ function LoadingPage() {
         <h1>Open Dota Project</h1>
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<LoadUserData />}></Route>
-                <Route path="/:id" element={<LoadUserData />}></Route>
+                <Route path="/" element={<HomePage/>}></Route>
+                <Route path="/player/:id" element={<LoadUserData />}></Route>
                 <Route path="*" element={<p>404 Page Not Found</p>} />
             </Routes>
         </BrowserRouter>
