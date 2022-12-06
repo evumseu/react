def121723526c71cb11ade4857e77d0ef9b8a823
evumseu <evum95@gmail.com>
2022-12-06 22:51:02 +0800

    swj done add app bar

--- src/LoadingPage.js
+++ src/LoadingPage.js
@@ -1,15 +1,18 @@
 import { SEU_USER_ID, ProfileData } from "./OpenDotaDB"
-import { BrowserRouter, Routes, Route, useParams, useNavigate, json, useRouteLoaderData } from "react-router-dom"
+import { BrowserRouter, Routes, Route, useParams, useNavigate, NavLink, Navigate, useRouteLoaderData } from "react-router-dom"
 import * as React from 'react';
-import { useState, useEffect, useLayoutEffect } from "react"
-import { TextField, Button, Box, Card, CardActions, CardContent, Typography} from "@mui/material"
+import { useState, useEffect, useLayoutEffect, MouseEvent } from "react"
+import { TextField, Button, Box, Card, CardActions, CardContent, Typography } from "@mui/material"
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import './LoadingPage.css';
 
 function LoadUserData() {
 
     const navigate = useNavigate()
     const params = useParams()
     const [profileData, setProfileData] = useState(ProfileData)
-    const [id, setID] = useState(params.queryId ? params.queryId : '')
+    const [id, setID] = useState(params.queryId ? params.queryId : SEU_USER_ID)
 
 
     const handleSubmit = (event) => {
@@ -23,12 +26,11 @@ function LoadUserData() {
 
     const bull = (
         <Box
-          component="span"
-          sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+            component="span"
+            sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
         >
-          •
         </Box>
-      );
+    );
 
     useEffect(() => {
         fetch(`https://api.opendota.com/api/players/${id}`).then((Response) => Response.json()).then((data) => {
@@ -51,27 +53,27 @@ function LoadUserData() {
             <Button variant="contained" size="large" onClick={handleSubmit}>Search</Button>
 
             <div>
-            <React.Fragment>
-    <CardContent>
-      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        Word of the Day
-      </Typography>
-      <Typography variant="h5" component="div">
-        {bull}nev{bull}o{bull}lent
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        adjective
-      </Typography>
-      <Typography variant="body2">
-        well meaning and kindly.
-        <br />
-        {'"a benevolent smile"'}
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button size="small">Learn More</Button>
-    </CardActions>
-  </React.Fragment>
+                <React.Fragment>
+                    <CardContent variant="outlined">
+                        <Typography mt={2} variant="h4" color="text.primary" gutterBottom>
+                            Player Name: {profileData.profile.personaname}
+                        </Typography>
+                        <Typography variant="h5" component="div">
+                            {bull}nev{bull}o{bull}lent
+                        </Typography>
+                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                            adjective
+                        </Typography>
+                        <Typography variant="body2">
+                            well meaning and kindly.
+                            <br />
+                            {'"a benevolent smile"'}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Learn More</Button>
+                    </CardActions>
+                </React.Fragment>
                 <h2 >Player Name: {profileData.profile.personaname}</h2>
                 <h2>MMR : {profileData.mmr_estimate.estimate}</h2>
                 <img src={profileData.profile.avatar}></img>
@@ -90,9 +92,11 @@ function HomePage() {
 }
 
 function LoadingPage() {
+
     return <div>
-        <h1>Open Dota Project</h1>
         <BrowserRouter>
+            <TopAppHeader />
+            <br /><br />
             <Routes>
                 <Route path="/" element={<HomePage />}></Route>
                 <Route path="/player/:queryId" element={<LoadUserData />}></Route>
@@ -101,5 +105,48 @@ function LoadingPage() {
         </BrowserRouter>
     </div>
 }
+
+function TopAppHeader() {
+    const navigate = useNavigate()
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}>
+                        Dota2
+                    </Typography>
+                    {"   "}
+                    <Button color="inherit" onClick={console.log("This is match")}>Match</Button>
+                    <Button color="inherit"><NavLink
+                        to={`player/${SEU_USER_ID}`}
+                        style={{
+                            color:'white',
+                            textDecoration: 'none',
+                        }}>
+                        Player
+                    </NavLink></Button>
+                    <Button color="inherit"><NavLink to>Top Player</NavLink></Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
+
+
+
+
 
 export default LoadingPage
