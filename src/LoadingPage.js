147c150e338e5b425ad0102f08216a1f4f900ac2
evumseu <evum95@gmail.com>
2022-12-08 23:32:48 +0800

    swj develop data grid

--- src/LoadingPage.js
+++ src/LoadingPage.js
@@ -1,10 +1,12 @@
-import { SEU_USER_ID, ProfileData, WinLost, RecentMatches} from "./OpenDotaDB"
-import { BrowserRouter, Routes, Route, useParams, useNavigate, NavLink, Navigate, useRouteLoaderData } from "react-router-dom"
+import { HeroDataBase } from "./HeroDataBase"
+import { SEU_USER_ID, ProfileData, WinLost, RecentMatches } from "./OpenDotaDB"
+import { BrowserRouter, Routes, Route, useParams, useNavigate, NavLink } from "react-router-dom"
 import * as React from 'react';
-import { useState, useEffect, useLayoutEffect, MouseEvent } from "react"
+import { useState, useEffect } from "react"
 import { TextField, Button, Box, Avatar, Card, CardActions, CardContent, Typography, AppBar, Toolbar } from "@mui/material"
 import { DataGrid } from '@mui/x-data-grid';
 import './LoadingPage.css';
+import { getAutoHeightDuration } from "@mui/material/styles/createTransitions";
 
 function LoadUserData() {
 
@@ -32,14 +34,8 @@ function LoadUserData() {
 
         fetch(`https://api.opendota.com/api/players/${id}/wl`).then((Response) => Response.json()).then((data) => {
             setWinLost(data)
-           
-        })
 
-        fetch(`https://api.opendota.com/api/players/${id}/recentMatches`).then((Response) => Response.json()).then((data) => {
-            //setRecentMatches(data)
-            console.log(data)
         })
-
 
     }, [params.queryId]);
 
@@ -87,7 +83,7 @@ function LoadUserData() {
                     </Typography>
                 </div>
 
-                <MatchesTable matches={recentMatches} />
+                <MatchesTable matches={recentMatches} id={id} />
 
             </div>
         </form>
@@ -140,7 +136,7 @@ function TopAppHeader() {
                         Dota2
                     </Typography>
                     {"   "}
-                    <Button color="inherit" onClick={console.log("This is match")}>Match</Button>
+                    <Button color="inherit">Match</Button>
                     <Button color="inherit"><NavLink
                         to={`player/${SEU_USER_ID}`}
                         style={{
@@ -156,104 +152,125 @@ function TopAppHeader() {
     );
 }
 
+
+
 function MatchesTable(props) {
 
-var matchData = [];
+    function getTeam(params) {
+        if(params.value > 127){
+            return "Dire"
+        } else {
+            return "Radiant"
+        }
+    }
 
-    const match = [
-        {
-            "match_id": 6906115589,
-            "player_slot": 4,
-            "radiant_win": false,
-            "duration": 2985,
-            "game_mode": 22,
-            "lobby_type": 0,
-            "hero_id": 110,
-            "start_time": 1670419875,
-            "version": null,
-            "kills": 1,
-            "deaths": 20,
-            "assists": 10,
-            "skill": null,
-            "average_rank": 33,
-            "xp_per_min": 424,
-            "gold_per_min": 289,
-            "hero_damage": 21530,
-            "tower_damage": 104,
-            "hero_healing": 48,
-            "last_hits": 132,
-            "lane": null,
-            "lane_role": null,
-            "is_roaming": null,
-            "cluster": 151,
-            "leaver_status": 0,
-            "party_size": 1
-          },
-    ]
+    function getResult(params) {
+        if(params.row.player_slot > 127){
+            if(params.value){
+                return "Lost"
+            } else {
+                return "Win"
+            }
+        } else {
+            if(params.value){
+                return "Win"
+            } else {
+                return "Lost"
+            }
+        }
+    }
+
+    function getHero(params) {
+        for(var i = 0; i < HeroDataBase.length; i++){
+            if (params.value === HeroDataBase[i].id){
+               return  HeroDataBase[i].localized_name
+            }
+        }
+    } 
+
+    function getHeroAvatar(params) {
+        for(var i = 0; i < HeroDataBase.length; i++){
+            if (params.value === HeroDataBase[i].id){
+                var name = ''+ HeroDataBase[i].localized_name
+                var result = name.toLocaleLowerCase().replace(/ /g, '')
+                var link = "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/"+result+".png" 
+                return link
+            }
+        }
+        
+    }
+
+    const [recentMatches, setRecentMatches] = useState(RecentMatches);
 
     const column = [
-        { field: 'hero_id', headerName: 'Hero', width: 130 },
-        { field: 'player_slot', headerName: 'Team', width: 130},
-        { field: 'radiant_win', headerName: 'Result', width: 70 },
-        { field: 'kills', headerName: 'Kills', width: 70 },
-        { field: 'deaths', headerName: 'Deaths', width: 70 },
-    ];
-
-    const columns = [
-        { field: 'id', headerName: 'ID', width: 70 },
-        { field: 'firstName', headerName: 'First name', width: 130 },
-        { field: 'lastName', headerName: 'Last name', width: 130 },
-        {
-          field: 'age',
-          headerName: 'Age',
-          type: 'number',
-          width: 90,
+        { 
+            field: 'hero_id', 
+            headerName: 'Hero', 
+            width: 200,
+            renderCell: (params) => {
+                return (
+                  <div className="tableAvatar">
+                    <Avatar sx={{mr: 2}} src={getHeroAvatar(params)} /> {getHero(params)}
+                  </div>
+                );
+              }
         },
         {
-          field: 'fullName',
-          headerName: 'Full name',
-          description: 'This column has a value getter and is not sortable.',
-          sortable: false,
-          width: 160,
-          valueGetter: (params) =>
-            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+            field: 'player_slot',
+            headerName: 'Team',
+            width: 130,
+            valueGetter: getTeam,
         },
-      ];
+        { 
+            field: 'radiant_win', 
+            headerName: 'Result', 
+            width: 130,
+            valueGetter: getResult, 
+        },
+        { field: 'kills', headerName: 'Kills', width: 130 },
+        { field: 'deaths', headerName: 'Deaths', width: 130 },
+        { field: 'assists', headerName: 'Assists', width: 130 },
+        { 
+            field: 'average_rank', 
+            headerName: 'Average Rank', 
+            width: 130 
+        },
 
-    const rows = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
     ];
 
-    if (!props.matches){
-        matchData = props.matches
-    } else {
-        matchData = match;
-    }
+    useEffect(() => {
+
+        fetch(`https://api.opendota.com/api/players/${props.id}/recentMatches`).then((Response) => Response.json()).then((data) => {
+            setRecentMatches(data)
+        })
+
+
+    }, [props.id]);
 
     return (
         <center>
             <div className="table">
-               {console.log("This is here" + props.matches)} 
                 <DataGrid
-                    rows={props.matches}
+                    rows={recentMatches}
                     columns={column}
-                    pageSize={5}
+                    pageSize={10}
                     rowsPerPageOptions={[5]}
-                    getRowId={(row) =>  row.match_id}
+                    getRowId={(row) => generateRandom()}
                 />
             </div>
         </center>
     );
 }
 
+function generateRandom() {
+    var length = 8,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
 
 
 
